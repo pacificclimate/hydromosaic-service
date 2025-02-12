afe16d0357067213bb8e70f8812a01aaed5c5a9c
Lee Zeman <lzeman@uvic.ca>
2025-02-11 19:36:35 -0800

    mockup API to support frontend development

---
+++ hms/api/timeseries.py
@@ -0,0 +1,216 @@
+def list_timeseries(subid):
+    """Return dummy data for now"""
+    return (
+        [
+            {
+                "id": "1",
+                "uri": "/outlets/sub4008275/timeseries/1",
+                "data_uri": "/outlets/sub4008275/timeseries/1/data",
+                "variable": "q_in",
+                "variable_uri": "/variables/q_in",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "2",
+                "uri": "/outlets/sub4008275/timeseries/2",
+                "data_uri": "/outlets/sub4008275/timeseries/2/data",
+                "variable": "q_sim",
+                "variable_uri": "/variables/q_sim",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "3",
+                "uri": "/outlets/sub4008275/timeseries/3",
+                "data_uri": "/outlets/sub4008275/timeseries/3/data",
+                "variable": "q_obs",
+                "variable_uri": "/variables/q_obs",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "4",
+                "uri": "/outlets/sub4008275/timeseries/4",
+                "data_uri": "/outlets/sub4008275/timeseries/4/data",
+                "variable": "q_in",
+                "variable_uri": "/variables/q_in",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "5",
+                "uri": "/outlets/sub4008275/timeseries/5",
+                "data_uri": "/outlets/sub4008275/timeseries/5/data",
+                "variable": "q_sim",
+                "variable_uri": "/variables/q_sim",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "6",
+                "uri": "/outlets/sub4008275/timeseries/6",
+                "data_uri": "/outlets/sub4008275/timeseries/6/data",
+                "variable": "q_obs",
+                "variable_uri": "/variables/q_obs",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "CanESM2",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "7",
+                "uri": "/outlets/sub4008275/timeseries/7",
+                "data_uri": "/outlets/sub4008275/timeseries/7/data",
+                "variable": "q_in",
+                "variable_uri": "/variables/q_in",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "8",
+                "uri": "/outlets/sub4008275/timeseries/8",
+                "data_uri": "/outlets/sub4008275/timeseries/8/data",
+                "variable": "q_sim",
+                "variable_uri": "/variables/q_sim",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "9",
+                "uri": "/outlets/sub4008275/timeseries/9",
+                "data_uri": "/outlets/sub4008275/timeseries/9/data",
+                "variable": "q_obs",
+                "variable_uri": "/variables/q_obs",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp26",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "10",
+                "uri": "/outlets/sub4008275/timeseries/10",
+                "data_uri": "/outlets/sub4008275/timeseries/10/data",
+                "variable": "q_in",
+                "variable_uri": "/variables/q_in",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "11",
+                "uri": "/outlets/sub4008275/timeseries/11",
+                "data_uri": "/outlets/sub4008275/timeseries/11/data",
+                "variable": "q_sim",
+                "variable_uri": "/variables/q_sim",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            {
+                "id": "12",
+                "uri": "/outlets/sub4008275/timeseries/12",
+                "data_uri": "/outlets/sub4008275/timeseries/12/data",
+                "variable": "q_obs",
+                "variable_uri": "/variables/q_obs",
+                "outlet": "sub4008275",
+                "outlet_uri": "/outlets/sub4008275",
+                "scenario": "rcp85",
+                "model": "PCIC6",
+                "start_time": "1991-01-01 00:00:00",
+                "end_time": "2001-01-01 00:00:00",
+                "num_times": 3653
+                },
+            ]
+        )
+    
+    
+    
+def timeseries(subid, ts_id):
+    """Return dummy data for now"""
+    return(
+        {
+            "id": "1",
+            "uri": "/outlets/sub4008275/timeseries/1",
+            "data_uri": "/outlets/sub4008275/timeseries/1/data",
+            "variable": "q_in",
+            "variable_uri": "/variables/q_in",
+            "outlet": "sub4008275",
+            "outlet_uri": "/outlets/sub4008275",
+            "scenario": "rcp26",
+            "model": "CanESM2",
+            "start_time": "1991-01-01 00:00:00",
+            "end_time": "2001-01-01 00:00:00",
+            "num_times": 3653
+            }
+        )
+    
+    
+def timeseries_data(subid, ts_id):
+    """Return dummy data for now"""
+    return("""
+Attribute, Value
+Variable, q_in
+Outlet, sub4008275
+Scenario, rcp26
+Model, CanESM2
+
+time (hours since 1991-01-01 00:00:00), q_in (m**3 s**-1)
+0, 0.000146036960985626
+24, 0.0750794376526951
+48, 9.148511994179339
+72, 0.145543980219365
+96, 0.142641812389788
+120, 0.139802770939621
+144, 0.137024598885108
+"""
+)
---
+++ hms/asgi.py
@@ -0,0 +1,3 @@
+from hms import create_app
+
+connexion_app, flask_app = create_app()
