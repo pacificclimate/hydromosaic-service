59f8d9e9307475508b2147e2f825ea0bd50f7900
Lee Zeman <lzeman@uvic.ca>
2025-02-12 17:01:41 -0800

    format with black

--- hms/__init__.py
+++ hms/__init__.py
@@ -4,15 +4,16 @@ from flask_cors import CORS
 connexion_app = None
 flask_app = None
 
+
 def create_app():
     global connexion_app, flask_app
     connexion_app = connexion.FlaskApp(__name__, specification_dir="openapi/")
     flask_app = connexion_app.app
-    
+
     CORS(flask_app)
-    
+
     ## database initialization will go here.
-    
+
     connexion_app.add_api("api-spec.yaml")
-    
+
     return connexion_app, flask_app
