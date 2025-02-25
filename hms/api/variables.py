f073a912c97d56966585bccaae499e9fb17582ca
Lee Zeman <lzeman@uvic.ca>
2025-02-25 14:11:04 -0800

    return variables from database

--- hms/api/variables.py
+++ hms/api/variables.py
@@ -1,32 +1,29 @@
+from hydromosaic.database import Variable
+from hms import get_app_session
+
 def list_variables():
-    """For now, return dummy values"""
-    return [
-        {
-            "id": "q_in",
-            "uri": "/variables/q_in",
-            "units": "m**3 s**-1",
-            "long_name": "Simulated reservoir inflows",
-        },
-        {
-            "id": "q_sim",
-            "uri": "/variables/q_sim",
-            "units": "m**3 s**-1",
-            "long_name": "Simulated outflows",
-        },
-        {
-            "id": "q_obs",
-            "uri": "/variables/q_obs",
-            "units": "m**3 s**-1",
-            "long_name": "Observed outflows",
-        },
-    ]
+    """return a list with an object for each variable in the database"""
+    vars = get_app_session().query(Variable).all()
+    
+    return( [
+        {"id": v.standard_name,
+         "uri": f"/variables/{v.standard_name}",
+         "units": v.units,
+         "long_name": v.long_name} 
+        for v in vars])
 
 
 def variable(v_id):
-    """For now, return dummy values"""
-    return {
-        "id": "q_in",
-        "uri": "/variables/q_in",
-        "units": "m**3 s**-1",
-        "long_name": "Simulated reservoir inflows",
-    }
+    """Return an object representing the requested variable"""
+    
+    try:
+        var = get_app_session().query(Variable).filter_by(standard_name = v_id).one()
+    
+        return {
+            "id": var.standard_name,
+            "uri": f"/variables/{var.standard_name}",
+            "units": var.units,
+            "long_name": var.long_name,
+            } 
+    except:
+        return {}
