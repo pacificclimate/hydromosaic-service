1e7c197a6f9757efacbf4b4a26531e3613cae27a
Lee Zeman <lzeman@uvic.ca>
2025-03-14 15:10:24 -0700

    convert data to timestamps

--- hms/api/data.py
+++ hms/api/data.py
@@ -1,10 +1,15 @@
 from hydromosaic.database import Outlet, Timeseries, Variable, Scenario, Model, Datafile
 from netCDF4 import Dataset
 from hms import get_app_session
+from datetime import datetime, timedelta
 
+
+def time_string(reference, hours):
+	return (reference + timedelta(hours = hours)).strftime("%Y-%m-%d %H:%M:%S")
+	
 
 def timeseries_data(subid, ts_id):
-    """Return dummy data for now"""
+    """Return a timeseries in CSV format, one metadata table and one timeseries table"""
 
     # Get the metadata from the database, build metadata table
     ts = get_app_session().query(Timeseries).filter_by(id=ts_id).one()
@@ -25,29 +30,13 @@ def timeseries_data(subid, ts_id):
     df = get_app_session().query(Datafile).filter_by(id=ts.datafile_id).one()
     nc = Dataset(df.filename)
 
-    header = f"Time ({nc.variables['time'].units}), {var.standard_name} ({nc.variables[var.standard_name].units})\n"
+    header = f"Time, {var.standard_name} ({nc.variables[var.standard_name].units})\n"
 
     outlet_index = nc.variables["basin_name"][:].tolist().index(subid)
-    timestamps = nc.variables["time"][:]
+    reference_time = datetime.strptime(nc.variables['time'].units, "hours since %Y-%m-%d %H:%M:%S")
+    timestamps = [time_string(reference_time, t) for t in nc.variables["time"][:]]
     data = nc.variables[var.standard_name][0 : len(timestamps), outlet_index]
 
     data_rows = [f"{timestamps[i]}, {data[i]}" for i in range(len(timestamps))]
 
     return metadata + header + "\n".join(data_rows)
-
-    return """
-Attribute, Value
-Variable, q_in
-Outlet, sub4008275
-Scenario, rcp26
-Model, CanESM2
-
-time (hours since 1991-01-01 00:00:00), q_in (m**3 s**-1)
-0, 0.000146036960985626
-24, 0.0750794376526951
-48, 9.148511994179339
-72, 0.145543980219365
-96, 0.142641812389788
-120, 0.139802770939621
-144, 0.137024598885108
-"""
